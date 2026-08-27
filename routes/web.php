64c527e4b23ea9bd72ae003b012f6da02887510f
Orjihenry <orjihen@yahoo.com>
2026-08-26 23:17:06 -0230

    added new routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 Route::inertia('/our-story', 'our-story')->name('our-story');
+Route::inertia('/bread-and-specialties', 'bread-and-specialties')->name('bread-and-specialties');
+Route::inertia('/contact', 'contact')->name('contact');
+
+Route::inertia('/shop', 'shop')->name('shop');
+Route::inertia('/featured', 'featured')->name('featured');
+Route::inertia('/privacy-policy', 'privacy-policy')->name('privacy-policy');
+Route::inertia('/terms-of-service', 'terms-of-service')->name('terms-of-service');
+Route::inertia('/cookie-policy', 'cookie-policy')->name('cookie-policy');
 
 Route::prefix('{current_team}')
     ->middleware(['auth', 'verified', EnsureTeamMembership::class])
