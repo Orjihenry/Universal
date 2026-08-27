69bbe667b79db85fa15a21f078fd5125504689af
Orjihenry <orjihen@yahoo.com>
2026-08-26 22:25:28 -0230

    added page - Our Story

---
+++ resources/js/pages/our-story.tsx
@@ -0,0 +1,24 @@
+import SiteLayout from '@/layouts/site-layout';
+import { Head } from '@inertiajs/react';
+
+export default function OurStory() {
+    return (
+        <>
+            <Head title="Global Universal Bread Industry - Our Story" />
+            <main>
+                <section className="our-story">
+                    <div className="our-story-content">
+                        <h1>Our Story</h1>
+                        <p>
+                            Global Universal Bread Industry is a leading manufacturer of bread and related products.
+                            We have been in business for over 30 years and are proud to be a part of the community.
+                        </p>
+                        <p>
+                            We are a family-owned business and are committed to providing the best possible products and service to our customers.
+                        </p>
+                    </div>
+                </section>
+            </main>
+        </>
+    );
+}
